76f52962b9b8a3b52962dc604e6bb87109fc7629
Andrew Phillips <skeledrew@gmail.com>
2017-05-05 09:41:49 -0500

    Added PyShell

---
+++ pyshell.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+
+'''
+PyShell
+- A thin Python REPL providing additional functions
+- Addons:
+-- Session save/autosave/load
+-- Input history
+-- Output logging
+-- Init templates
+-- Allow multiline code correction and formatting
+'''
+
+
+from common import *
+
+
